b02535f0d0647df54d587bc6524e2d033f789331
Sergio Lagos Vergara <sergio.lagos@mail.udp.cl>
2021-12-10 17:22:13 -0300

    api

--- src/components/TarjetaBip.js
+++ src/components/TarjetaBip.js
@@ -1,40 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "antd/dist/antd.css";
-import { SearchOutlined, NumberOutlined} from '@ant-design/icons';
-import { Form, Input, Button } from 'antd';
 
+import { SearchOutlined, NumberOutlined } from "@ant-design/icons";
+import { message, Form, Input, Button, Spin } from "antd";
+
+import axios from "axios";
+import ReactLoading from "react-loading";
 
 function TarjetaBip() {
-
+  const [idTarjeta, setIdTarjeta] = useState();
+  const [saldo, setSaldo] = useState(0);
+  const [loadingSaldo, setLoadingSaldo] = useState(false);
+  // useEffect(()=>{
+  //
+  // })
+  console.log(idTarjeta);
+  const onFinish = (values) => {
+    setLoadingSaldo(true);
+    axios
+      .get("https://api.xor.cl/red/balance/".concat(values.numero_bip))
+      .then(function (response) {
+        message.success(response.data.status_description);
+        setLoadingSaldo(false);
+        setSaldo(response.data.balance);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
 
   return (
     <div className="background">
-
-      <div className="background-form">
-
-      </div>
-      <div className="borde">
-      </div>
+      <div className="background-form"></div>
+      <div className="borde"></div>
       <div className="bip">
         <div className="bip__content">
           <p className="bip-text">bip!</p>
         </div>
-        <div className="bip__number"> Saldo: $2500</div>
-      </div> 
+        <div className="bip__number">
+          {" "}
+          {loadingSaldo ? (
+            <div className="spinner">
+              <ReactLoading />
+            </div>
+          ) : (
+            "Saldo: " + saldo
+          )}
+        </div>
+      </div>
 
-      <div className = "form">
-        <Form>
-          <Form.Item>
-            <Input prefix={<NumberOutlined className="site-form-item-icon" style={{color:"grey"}} />} placeholder="Ingrese número de tarjeta"></Input>
+      <div className="form">
+        <Form onFinish={onFinish}>
+          <Form.Item name="numero_bip">
+            <Input
+              prefix={
+                <NumberOutlined
+                  className="site-form-item-icon"
+                  style={{ color: "grey" }}
+                />
+              }
+              placeholder="Ingrese número de tarjeta"
+            ></Input>
           </Form.Item>
           <Form.Item>
-            <Button type="primary" htmlType="submit" style={{background:"#06D6A0", borderColor: "#06D6A0"}}> Consultar saldo <SearchOutlined /></Button>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ background: "#06D6A0", borderColor: "#06D6A0" }}
+            >
+              {" "}
+              Consultar saldo <SearchOutlined />
+            </Button>
           </Form.Item>
         </Form>
       </div>
     </div>
-
-    
   );
 }
 
